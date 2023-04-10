62576a66928c5d67f1fdf5478f6e9472271733b2
daniel.ofir <daniel.ofir@imperva.com>
2023-04-10 10:35:10 +0300

    fixing formats

--- app.py
+++ app.py
@@ -7,16 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def generate_random():
 
-    possible_manipulations = ['square', 'double', 'triple']
-    random_numbers = []
-    for manipulation in possible_manipulations:
-        # Send a POST request to the second microservice with the random number
-        num = random.randint(1, 1000)
-        random_numbers.append(num)
-    res = requests.post('http://manipulator-stage:80/manipulator', json={'nums': random_numbers})
+    # Send a POST request to the second microservice with the random number
+    num = random.randint(1, 1000)
+    res = requests.post('http://manipulator-stage:80/manipulator', json={'num': num})
     outcome = res.json()
 
-    return render_template('index.html', nums=outcome)
+    return render_template('index.html', num=outcome)
 
 
 if __name__ == '__main__':
